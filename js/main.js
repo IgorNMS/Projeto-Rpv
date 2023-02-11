d4096c65c4bdec1655ec9ca37ad8943a694bbfc6
Igor <nascimentoigor49@gmail.com>
2023-02-10 22:51:18 -0300

    Fix: Correção pequena no contador.

--- js/main.js
+++ js/main.js
@@ -114,45 +114,55 @@
     },
   });
 
-  /* contador
+  // contador
+  if (window.location.pathname == "/ingressos.html") {
     (function () {
-        const second = 1000,
-              minute = second * 60,
-              hour = minute * 60,
-              day = hour * 24;
-        let today = new Date(),
-            dd = String(today.getDate()).padStart(2, "0"),
-            mm = String(today.getMonth() + 1).padStart(2, "0"),
-            yyyy = today.getFullYear(),
-            nextYear = yyyy + 1,
-            dayMonth = "12/1/",
-            birthday = dayMonth + yyyy;
-        
-        today = mm + "/" + dd + "/" + yyyy;
-        if (today > birthday) {
-          birthday = dayMonth + nextYear;
-        }
-        //end
-        
-        const countDown = new Date(birthday).getTime(),
-            x = setInterval(function() {    
-      
-              const now = new Date().getTime(),
-                    distance = countDown - now;
-      
-              document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-      
-              if (distance < 0) {
-                document.getElementById("headline").innerText = "O dia do evento chegou!!";
-                document.getElementById("countdown").style.display = "none";
-                document.getElementById("content").style.display = "block";
-                clearInterval(x);
-              }
-              //seconds
-            }, 0)
-        }());
-     contador */
+      const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+      let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "12/1/",
+        birthday = dayMonth + yyyy;
+
+      today = mm + "/" + dd + "/" + yyyy;
+      if (today > birthday) {
+        birthday = dayMonth + nextYear;
+      }
+      //end
+
+      const countDown = new Date(birthday).getTime(),
+        x = setInterval(function () {
+          const now = new Date().getTime(),
+            distance = countDown - now;
+
+          (document.getElementById("days").innerText = Math.floor(
+            distance / day
+          )),
+            (document.getElementById("hours").innerText = Math.floor(
+              (distance % day) / hour
+            )),
+            (document.getElementById("minutes").innerText = Math.floor(
+              (distance % hour) / minute
+            )),
+            (document.getElementById("seconds").innerText = Math.floor(
+              (distance % minute) / second
+            ));
+
+          if (distance < 0) {
+            document.getElementById("headline").innerText =
+              "O dia do evento chegou!!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0);
+    })();
+  }
+  //contador
 })(jQuery);
